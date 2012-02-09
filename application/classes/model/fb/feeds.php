6972dcc923361f5fcf8585fc0fe22a5a908f3fcd
Muntasir Mohiuddin <muntasir.mohiuddin@gmail.com>
2012-02-09 12:42:29 -0500

    adding models

---
+++ application/classes/model/fb/feeds.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct script access.'); 
+
+/**
+ * ORM fb_users Model.
+ * 
+ * Provides access to the employees data as stored in the database 
+ * 
+ * @category Models
+ * @author BNOTIONS
+ */
+
+class Model_Fb_Feeds extends ORM {
+	protected $_table_name = 'fb_feeds';
+}
