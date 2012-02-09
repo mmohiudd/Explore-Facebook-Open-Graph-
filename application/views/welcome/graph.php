dd2354ae49df9c410c10a596e001dd788ab7fee3
Muntasir Mohiuddin <muntasir.mohiuddin@gmail.com>
2012-02-09 14:08:09 -0500

    adding missing view files

---
+++ application/views/welcome/graph.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<!--[if lte IE 8 ]> 
+	<html lang="en" class="no-js oldie"> <![endif]-->
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta http-equiv="imagetoolbar" content="no">
+						
+		<title>Explore Facebook Open Graph with BNOTIONS</title>
+		
+		<meta name="description" content="">
+		<meta name="author" content="(c) 2012 - BNOTIONS">
+		
+		<link rel="shortcut icon" type="image/png" href="<?php echo url::site()?>assets/img/favicon.png">
+		
+		<link rel="stylesheet" href="<?php echo url::site()?>assets/css/bp.css">
+		<link rel="stylesheet" href="<?php echo url::site()?>assets/css/screen.css?v=2" media="screen">
+	
+		<script src="<?php echo url::site()?>assets/js/libs/modernizr-2.0.6.min.js"></script>
+	
+	<!--[if (gte IE 6)&(lte IE 8)]>
+		<script type="text/javascript" src="<?php echo url::site()?>assets/js/libs/selectivizr-1.0.2.min.js"></script>
+	<![endif]-->	
+	</head>
+	
+	<body>
+		<header>
+			<h1></h1>
+		</header>
+	
+		<section id="ctas">
+			GRAPH GOES HERE
+		</section>
+		
+		<footer>
+			<div>
+				
+				<section></section>
+			</div>
+		</footer>
+	
+		<!-- scripts -->
+		<!-- 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="assets/js/libs/jquery-1.7.1.min.js"><\/script>')</script> -->
+		<script src="<?php echo url::site()?>assets/js/libs/jquery-1.7.1.min.js"></script>
+		<script src="<?php echo url::site()?>assets/js/plugins.js"></script>
+		<script src="<?php echo url::site()?>assets/js/script.js"></script>
+		
+		<script>
+			var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']]; // Change UA-XXXXX-X to be your site's ID
+			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
+			g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+			s.parentNode.insertBefore(g,s)}(document,'script'));
+		</script>
+	</body>
+</html>
