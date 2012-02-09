0bfa70af6e405cc83ba03913143505976048022f
Muntasir Mohiuddin <muntasir.mohiuddin@gmail.com>
2012-02-09 13:22:00 -0500

    adding thank you logic

--- application/classes/controller/welcome.php
+++ application/classes/controller/welcome.php
@@ -1,20 +1,40 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Controller_Welcome extends Controller_Grandma_Base{
-	
-	public function thankyou(){
+	// declare models
+	protected $fb_users;
+	protected $fb_feeds;
+		
+
+	public function before() {
+		parent::before();
+		
+		// load ORMs
+		$this->fb_users = ORM::factory("fb_users");
+		$this->fb_feeds = ORM::factory("fb_feeds");
+	}
+
+
+	public function action_graph(){
+		$view = View::factory('welcome/index');
+		
+		$login_url = url::site("welcome/graph");
+		$view->bind('login_url', $login_url);
+		
+		// Render the view
+		$page = $view->render()	;
+		
+		$this->response->body($page);
+		
 	}
 	
+
 	public function action_index() {
 		$facebook_config = Kohana::$config->load('facebook')->{Kohana::$environment};
-		$fb_user = $this->facebook->me();
+		
 		
 		try{
-			// load model 
-			$fb_users = ORM::factory("fb_users");
-			$fb_feeds = ORM::factory("fb_feeds");
-			
-			
+			// load models 
 			
 			$batch_data = array(
 				array(
@@ -34,18 +54,30 @@ class Controller_Welcome extends Controller_Grandma_Base{
 			// save the user data
 			$me = $results[0];
 			
-			if(!$fb_users->where('fb_uid', '=', $me['id'])->find_all()) {
-				$fb_users->fb_uid = $me['id'];
-				$fb_users->first_name = $me['first_name'];
-				$fb_users->last_name = $me['last_name'];
+			if(!$this->fb_users->where('fb_uid', '=', $me['id'])->find_all()) {
+				$this->fb_users->fb_uid = $me['id'];
+				$this->fb_users->first_name = $me['first_name'];
+				$this->fb_users->last_name = $me['last_name'];
 				
-				$fb_users->save();
+				$this->fb_users->save();
 			}
 			
-			$view = View::factory('welcome/graph');
+			$view = View::factory('welcome/thankyou');
+			
+			$graph_url = url::site("welcome/graph");
+			$view->bind('login_url', $graph_url);
+			
+			// Render the view
+			$page = $view->render()	;
+			
+			$this->response->body($page);
+			
 			
 		} catch(Exception $e) {
-			echo $e . "<br>";	
+			//echo "<!-- ";
+			echo $e;
+			//echo " -->";
+
 			$view = View::factory('welcome/index');
 			
 			$login_url = $this->facebook->getLoginUrl() . "&scope=" . implode(",", $facebook_config['permissions']);
