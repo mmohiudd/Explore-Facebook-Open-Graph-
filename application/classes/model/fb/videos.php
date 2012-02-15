f7e2fe8889f81ee4982efd6b9a0abc70c87630db
Muntasir Mohiuddin <muntasir.mohiuddin@gmail.com>
2012-02-15 16:24:06 -0500

    save video information

---
+++ application/classes/model/fb/videos.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.'); 
+
+/**
+ * ORM fb_users Model.
+ * 
+ * Provides access to the employees data as stored in the database 
+ * 
+ * @category Models
+ * @author BNOTIONS
+ */
+
+class Model_Fb_Videos extends ORM {
+	protected $_table_name = 'fb_videos
+	';
+}
