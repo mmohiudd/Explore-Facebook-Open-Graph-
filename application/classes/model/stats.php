f29a9e27d85b7745a5e6286e0e7d1a90c28a214a
Muntasir Mohiuddin <muntasir.mohiuddin@gmail.com>
2012-02-16 13:12:36 -0500

    adding updates to stats mdoel

--- application/classes/model/stats.php
+++ application/classes/model/stats.php
@@ -46,7 +46,7 @@ class Model_Stats extends Model_Database {
 	
 	
 	public function most_popular_video() {
-		$query = DB::select(DB::expr("COUNT(DISTINCT fb_id) AS c, id, fb_id, fb_uid, name"))->from('fb_videos')->group_by('fb_id')->order_by('c', "DESC");
+		$query = DB::select(DB::expr("COUNT(DISTINCT fb_uid) AS c, id, fb_id, fb_uid, name"))->from('fb_videos')->group_by('fb_uid')->order_by('c', "DESC");
 		
 		$results = $query->execute($this->_db);
 		
